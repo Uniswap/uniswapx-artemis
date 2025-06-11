21a8831a3a293baadc69eccae04823b18c4e1595
phanhoc <phanhocit@gmail.com>
2025-06-11 11:41:57 +0700

    fix decode

--- src/collectors/uniswapx_route_collector.rs
+++ src/collectors/uniswapx_route_collector.rs
@@ -145,6 +145,7 @@ pub struct OrderRoute {
     pub gas_use_estimate_quote: String,
     pub gas_use_estimate: String,
     pub route: Vec<Vec<Route>>,
+    #[serde(default)]
     pub method_parameters: Option<MethodParameters>,
 }
 
