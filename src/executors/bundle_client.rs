16e721a58917df08cf3f284b578d6f707cf8f9a1
Alan Wu <alanwu100@gmail.com>
2025-06-17 15:26:41 -0400

    feat: use struct for bundle params

--- src/executors/bundle_client.rs
+++ src/executors/bundle_client.rs
@@ -9,10 +9,20 @@ use alloy::{
     hex,
 };
 use anyhow::Result;
-use serde_json::{json, Value};
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
 use tracing::info;
 
 const HEX_PREFIX: &str = "0x";
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BundleParams {
+    pub txs: Vec<String>,
+    pub min_block_number: String,
+    pub max_block_number: String,
+    pub reverting_tx_hashes: Vec<String>,
+}
 
 pub struct BundleClient {
     sender_client: Arc<DynProvider<AnyNetwork>>,
@@ -41,18 +51,22 @@ impl BundleClient {
         let tx_hash_hex = format!("{}{}", HEX_PREFIX, hex::encode(tx_hash));
         
         // Build bundle params (single transaction bundle that's allowed to revert)
-        let params = json!({
-            "txs": vec![signed_tx],
-            "minBlockNumber": format!("{}{:x}", HEX_PREFIX, target_block),
-            "maxBlockNumber": format!("{}{:x}", HEX_PREFIX, target_block + bundle_window),
-            "revertingTxHashes": vec![tx_hash_hex],
-        });
+        let params = BundleParams {
+            txs: vec![signed_tx],
+            min_block_number: format!("{}{:x}", HEX_PREFIX, target_block),
+            max_block_number: format!("{}{:x}", HEX_PREFIX, target_block + bundle_window),
+            reverting_tx_hashes: vec![tx_hash_hex],
+        };
         
         info!("{} - Sending bundle for blocks {}-{}", order_hash, target_block, target_block + bundle_window);
+        info!("{} - Bundle params: {:?}", order_hash, params);
         
         // Send bundle
         let bundle_result = self.sender_client
-            .raw_request::<Vec<Value>, Value>(std::borrow::Cow::Borrowed("eth_sendBundle"), vec![params])
+            .raw_request::<Vec<Value>, Value>(
+                std::borrow::Cow::Borrowed("eth_sendBundle"), 
+                vec![serde_json::to_value(&params)?]
+            )
             .await?;
         
         // Extract bundle hash if available
