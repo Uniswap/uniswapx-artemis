ef3161f797283b16a66ae43728eae372920a799a
kienvc <17985897+kienvc@users.noreply.github.com>
2025-06-30 14:04:41 +0700

    add staging ci

--- ci/updateManifest.sh
+++ ci/updateManifest.sh
@@ -6,7 +6,7 @@ git clone "https://${PERSONAL_ACCESS_TOKEN}@${REPO_MANIFEST_URL}"
 cd ./${REPO_MANIFEST_NAME}
 git checkout ${REPO_MANIFEST_BRANCH} && git pull
 
-if [ ${GITHUB_REF_NAME} = "main" ]
+if [ ${GITHUB_REF_NAME} = "staging" ]
 then
   echo 'This is main branch'
   cd ${REPO_MANIFEST_ENV_MAIN}
